1305a53dfdc854ad1a897715b906f3379a7886bc
JJJ4032002 <rp4888888@gmail.com>
2022-12-01 02:58:58 +0530

    Add reactrouter and initial paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primereact/resources/themes/md-light-indigo/theme.css";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import SignIn from "./Components/SignIn/SignIn";
+import SignUp from "./Components/SignUp/SignUp";
 function App() {
+  let location = useLocation();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {location.pathname === "/" && <Navigate to="signin" />}
+      <Routes>
+        <Route path="signin" element={<SignIn></SignIn>} />
+        <Route path="signup" element={<SignUp></SignUp>} />
+      </Routes>
     </div>
   );
 }

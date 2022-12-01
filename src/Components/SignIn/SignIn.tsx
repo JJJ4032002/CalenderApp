d18c92015f8c534b37f44290cea01f523733d7d2
JJJ4032002 <rp4888888@gmail.com>
2022-12-02 02:53:06 +0530

    Add SignIn and SignUp

--- src/Components/SignIn/SignIn.tsx
+++ src/Components/SignIn/SignIn.tsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import bannerImage from "../../assets/BannerImg.svg";
 import Authentication from "../layouts/Authentication";
+import SignInUser from "../../firebase/SignInUser";
+import { Toast } from "primereact/toast";
+import { Link } from "react-router-dom";
+
 export default function SignIn() {
+  const [userData, setUserData] = useState({ email: "", password: "" });
+  let toast = useRef(null);
+  function handleUserData(name: "email" | "password", value: string) {
+    setUserData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  }
+
+  async function handleUserSignIn(email: string, password: string) {
+    let success = await SignInUser(email, password, toast);
+    return success;
+  }
   return (
-    <Authentication
-      AuthentictionStage="Sign In"
-      bannerImage={bannerImage}
-      welcomeTextBlock={
-        <div className="WelcomeText">
-          <h2>Welcome back !</h2>
-          <p>Don't have an account, Click here to Sign Up</p>
-        </div>
-      }
-    ></Authentication>
+    <>
+      <Authentication
+        AuthentictionStage="Sign In"
+        bannerImage={bannerImage}
+        welcomeTextBlock={
+          <div className="WelcomeText">
+            <h2>Welcome back !</h2>
+            <p>
+              Don't have an account, Click here to{" "}
+              <Link to="/signup">Sign Up</Link>
+            </p>
+          </div>
+        }
+        userData={userData}
+        handleUserData={handleUserData}
+        submitFunc={handleUserSignIn}
+      ></Authentication>
+      <Toast ref={toast} />
+    </>
   );
 }
--- src/Components/SignUp/SignUp.tsx
+++ src/Components/SignUp/SignUp.tsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import bannerImage2 from "../../assets/BannerImg2.svg";
 import Authentication from "../layouts/Authentication";
+import createUser from "../../firebase/CreateUser";
+import { Link } from "react-router-dom";
+import { Toast } from "primereact/toast";
 export default function SignUp() {
+  const [userData, setUserData] = useState({ email: "", password: "" });
+  let toast = useRef(null);
+  function handleUserData(name: "email" | "password", value: string) {
+    setUserData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  }
+  async function handleSignUp(email: string, password: string) {
+    let success = await createUser(email, password, toast);
+    return success;
+  }
   return (
-    <Authentication
-      AuthentictionStage="Sign Up"
-      bannerImage={bannerImage2}
-      welcomeTextBlock={
-        <div className="WelcomeText">
-          <h2>Welcome back !</h2>
-          <p>Don't have an account, Click here to Sign Up</p>
-        </div>
-      }
-    ></Authentication>
+    <>
+      <Authentication
+        AuthentictionStage="Sign Up"
+        bannerImage={bannerImage2}
+        welcomeTextBlock={
+          <div className="WelcomeText">
+            <h2>Welcome back !</h2>
+            <p>
+              Already have an account? Click here to{" "}
+              <Link to="/signin">Sign In</Link>
+            </p>
+          </div>
+        }
+        userData={userData}
+        handleUserData={handleUserData}
+        submitFunc={handleSignUp}
+      ></Authentication>
+      <Toast ref={toast} />
+    </>
   );
 }

c6ac3de9c43936f02846d501306a213ffd2282a8
JJJ4032002 <rp4888888@gmail.com>
2022-12-02 02:49:29 +0530

    Add Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
@@ -6,13 +6,20 @@ import "primereact/resources/themes/md-light-indigo/theme.css";
 import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import SignIn from "./Components/SignIn/SignIn";
 import SignUp from "./Components/SignUp/SignUp";
+import Dashboard from "./Components/Dashboard/Dashboard";
 function App() {
   let location = useLocation();
-
+  let [FirstLoad, setFirstLoad] = useState(true);
+  useEffect(() => {
+    if (FirstLoad) {
+      setFirstLoad(false);
+    }
+  }, [FirstLoad]);
   return (
     <div className="App">
-      {location.pathname === "/" && <Navigate to="signin" />}
+      {location.pathname === "/" && FirstLoad && <Navigate to="signin" />}
       <Routes>
+        <Route path="/" element={<Dashboard></Dashboard>} />
         <Route path="signin" element={<SignIn></SignIn>} />
         <Route path="signup" element={<SignUp></SignUp>} />
       </Routes>

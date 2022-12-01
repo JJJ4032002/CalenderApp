4042b54df7641f8060f905cd78081055fe98f532
JJJ4032002 <rp4888888@gmail.com>
2022-12-02 02:51:31 +0530

    Add SignOut button in navbar

--- src/Components/Elements/Navbar/Navbar.tsx
+++ src/Components/Elements/Navbar/Navbar.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./Navbar.css";
-export const Navbar = () => {
+import { useNavigate } from "react-router-dom";
+import { Button } from "primereact/button";
+import SignOutUser from "../../../firebase/SignOut";
+export const Navbar = ({ SignOutButton }: { SignOutButton: boolean }) => {
+  let navigate = useNavigate();
+  function handleSignOut() {
+    SignOutUser(navigate);
+  }
   return (
     <nav className="Nav">
       <h1>Arrange</h1>
+      {SignOutButton && <Button label="Sign Out" onClick={handleSignOut} />}
     </nav>
   );
 };
